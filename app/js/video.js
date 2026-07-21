696e05fbb1d2a44893614f740e05aa5335f45b11
Ruairi O'Brien <rtob@pm.me>
2026-07-20 22:06:40 -0500

    Update video.js

--- app/js/video.js
+++ app/js/video.js
@@ -2,22 +2,29 @@
 	var pauseButton = document.querySelector("#polina button");
 
 	function vidFade() {
-	  vid.classList.add("stopfade");
+	  if (vid) {
+	    vid.classList.add("stopfade");
+	  }
 	}
 
-	vid.addEventListener('ended', function() {
-	// only functional if "loop" is removed 
-	vid.pause();
-	// to capture IE10
-	vidFade();
-	}); 
-	pauseButton.addEventListener("click", function() {
-	  vid.classList.toggle("stopfade");
-	  if (vid.paused) {
-	    vid.play();
-	    pauseButton.innerHTML = "<i class='lni-pause'></i>";
-	  } else {
+	if (vid) {
+	  vid.addEventListener('ended', function() {
+	    // only functional if "loop" is removed 
 	    vid.pause();
-	    pauseButton.innerHTML = "<i class='lni-play'></i>";
-	  }
-	})
+	    // to capture IE10
+	    vidFade();
+	  }); 
+	}
+
+	if (pauseButton && vid) {
+	  pauseButton.addEventListener("click", function() {
+	    vid.classList.toggle("stopfade");
+	    if (vid.paused) {
+	      vid.play();
+	      pauseButton.innerHTML = "<i class='lni-pause'></i>";
+	    } else {
+	      vid.pause();
+	      pauseButton.innerHTML = "<i class='lni-play'></i>";
+	    }
+	  });
+	}
